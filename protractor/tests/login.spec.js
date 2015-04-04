d5abfbf29d98a3a574d8b8a15048069e4cd624ed
Andrew Eisenberg <andrew.eisenberg@tasktop.com>
2015-04-03 21:49:26 -0700

    Agile-Vancouver step 5-after
    Clear login dialog test implemented

--- protractor/tests/login.spec.js
+++ protractor/tests/login.spec.js
@@ -60,11 +60,21 @@ describe('LogIn', function () {
 
 
     it('should clear login dialog', function () {
-        // TODO: implement test
-        // 1. open login dialog
-        // 2. enter a user/pass
-        // 3. click the 'clear' button
-        // 4. use the getText method on #login-email and #login-password to ensure text is empty string
+        element(by.css('#login')).click();
+        browser.waitForAngular();
+
+        element(by.css('#login-email')).sendKeys('andrew.eisenberg@gmail.com');
+        element(by.css('#login-password')).sendKeys('nuthing');
+
+        element(by.css('button.clear')).click();
+
+        expect(element(by.cssContainingText('div.modal-header', 'Sign in')).isPresent()).toBeTruthy();
+        element(by.css('#login-email')).getText().then(function(text) {
+            expect(text).toBe('');
+        });
+        element(by.css('#login-password')).getText().then(function(text) {
+            expect(text).toBe('');
+        });
     });
 
 
