1426b10bdf4119bda1f49565d79a3325b75e1b91
Andrew Eisenberg <andrew.eisenberg@tasktop.com>
2015-04-03 22:15:17 -0700

    Agile-Vancouver step 6-middle
    LoginPage implemented, but Login spec untouched.

--- protractor/pages/login.page.js
+++ protractor/pages/login.page.js
@@ -3,47 +3,51 @@ var LoginPage = function() {};
 
 LoginPage.prototype = {
 	login : function(username, password) {
-		// TODO: fill in
+		this.openDialog();
+    	this.enterLoginDetails(username, password);
+		element(by.css('#do-login')).click();
 	},
 
 	logout : function() {
-		// TODO: fill in
+        element(by.css('#logout')).click();
 	},
 
 	openDialog : function() {
-		// TODO: fill in
+    	element(by.css('#login')).click();
+    	browser.waitForAngular();
 	},
 
 	enterLoginDetails : function(username, password) {
-		// TODO: fill in
+    	element(by.css('#login-email')).sendKeys(username);
+    	element(by.css('#login-password')).sendKeys(password);
 	},
 
 	cancelLogin : function() {
-		// TODO: fill in
+        element(by.css('button.cancel')).click();
 	},
 
 	clearLogin : function() {
-		// TODO: fill in
+        element(by.css('button.clear')).click();
 	},
 
 	getCurrentUsername : function() {
-		// TODO: fill in
+        return element(by.css('#login-email')).getText();
 	},
 
 	getCurrentPassword : function() {
-		// TODO: fill in
+        return element(by.css('#login-password')).getText();
 	},
 
 	hasError : function() {
-		// TODO: fill in
+		return element(by.css('.alert-error')).isPresent();
 	},
 
 	isLoggedIn : function() {
-		// TODO: fill in
+		return element(by.cssContainingText('a', 'Admin User')).isPresent();
 	},
 
 	isDialogOpen : function() {
-		// TODO: fill in
+		return element(by.cssContainingText('div.modal-header', 'Sign in')).isPresent();
 	}
 };
 module.exports = LoginPage;
