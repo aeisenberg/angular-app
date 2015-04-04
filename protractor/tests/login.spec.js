0dbfc5c7f48e42b4e1606da74c9dc6f0805305cd
Andrew Eisenberg <andrew.eisenberg@tasktop.com>
2015-04-04 09:13:26 -0700

    Agile-Vancouver step 6-after
    LoginPage implemented, and Login spec refactored to use it.

--- protractor/tests/login.spec.js
+++ protractor/tests/login.spec.js
@@ -9,89 +9,60 @@ describe('LogIn', function () {
     });
 
     it('should log in', function () {
-    	element(by.css('#login')).click();
-    	browser.waitForAngular();
+        loginPage.login('andrew.eisenberg@gmail.com', 'nuthing');
 
-    	element(by.css('#login-email')).sendKeys('andrew.eisenberg@gmail.com');
-    	element(by.css('#login-password')).sendKeys('nuthing');
-
-		element(by.css('#do-login')).click();
-
-    	expect(element(by.css('.alertError')).isPresent()).toBeFalsy();
-        expect(element(by.cssContainingText('a', 'Admin User')).isPresent()).toBeTruthy();
+        expect(loginPage.hasError()).toBeFalsy();
+        expect(loginPage.isLoggedIn()).toBeTruthy();
     });
 
 
     it('should log out', function () {
-    	element(by.css('#login')).click();
-    	browser.waitForAngular();
+        loginPage.login('andrew.eisenberg@gmail.com', 'nuthing');
+        loginPage.logout();
 
-    	element(by.css('#login-email')).sendKeys('andrew.eisenberg@gmail.com');
-    	element(by.css('#login-password')).sendKeys('nuthing');
-
-		element(by.css('#do-login')).click();
-
-    	expect(element(by.css('.alert-error')).isPresent()).toBeFalsy();
-        expect(element(by.cssContainingText('a', 'Admin User')).isPresent()).toBeTruthy();
-
-        element(by.css('#logout')).click();
-        expect(element(by.css('#logout')).isDisplayed()).toBeFalsy();
-        expect(element(by.cssContainingText('a', 'Admin User')).isPresent()).toBeFalsy();
+        expect(loginPage.hasError()).toBeFalsy();
+        expect(loginPage.isLoggedIn()).toBeFalsy();
     });
 
     it('should fail to log in with bad password', function () {
-        element(by.css('#login')).click();
-        browser.waitForAngular();
+        loginPage.login('andrew.eisenberg@gmail.com', 'not a password');
 
-        element(by.css('#login-email')).sendKeys('andrew.eisenberg@gmail.com');
-        element(by.css('#login-password')).sendKeys('not a password');
-
-        element(by.css('#do-login')).click();
-
-        expect(element(by.css('.alert-error')).isPresent()).toBeTruthy();
+        expect(loginPage.hasError()).toBeTruthy();
+        expect(loginPage.isLoggedIn()).toBeFalsy();
     });
 
     it('should close login dialog', function () {
-        element(by.css('#login')).click();
-        browser.waitForAngular();
+        loginPage.openDialog();
+        expect(loginPage.isDialogOpen()).toBeTruthy();
 
-        expect(element(by.cssContainingText('div.modal-header', 'Sign in')).isPresent()).toBeTruthy();
-        element(by.css('button.cancel')).click();
-        expect(element(by.cssContainingText('div.modal-header', 'Sign in')).isPresent()).toBeFalsy();
+        loginPage.cancelLogin();
+        expect(loginPage.isDialogOpen()).toBeFalsy();
     });
 
 
     it('should clear login dialog', function () {
-        element(by.css('#login')).click();
-        browser.waitForAngular();
+        loginPage.openDialog();
+        loginPage.enterLoginDetails('andrew.eisenberg@gmail.com', 'nuthing');
+        loginPage.clearLogin();
 
-        element(by.css('#login-email')).sendKeys('andrew.eisenberg@gmail.com');
-        element(by.css('#login-password')).sendKeys('nuthing');
-
-        element(by.css('button.clear')).click();
-
-        expect(element(by.cssContainingText('div.modal-header', 'Sign in')).isPresent()).toBeTruthy();
-        element(by.css('#login-email')).getText().then(function(text) {
-            expect(text).toBe('');
-        });
-        element(by.css('#login-password')).getText().then(function(text) {
-            expect(text).toBe('');
-        });
+        expect(loginPage.isDialogOpen()).toBeTruthy();
+        expect(loginPage.getCurrentUsername()).toBe('');
+        expect(loginPage.getCurrentPassword()).toBe('');
     });
 
 
     afterEach(function() {
         // logout if already logged in
-        element(by.css('#logout')).isDisplayed().then(function(result) {
+        loginPage.isLoggedIn().then(function(result) {
             if (result) {
-                element(by.css('#logout')).click();
+                loginPage.logout();
             }
         });
 
         // cancel login form if in the middle of logging in
-        element(by.css('button.cancel')).isPresent().then(function(result) {
+        loginPage.isDialogOpen().then(function(result) {
             if (result) {
-                element(by.css('button.cancel')).click();
+               loginPage.cancelLogin();
             }
         });
     });});
